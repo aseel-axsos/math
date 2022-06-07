c737b42c73afb0c709cf1aef1cf1aa57cc761437
Issa <aseel.issa@axsos.me>
2022-06-07 14:14:18 +0300

    first commit

---
+++ src/components/Mult.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Mult extends Component{
+    render(){
+        return (<div className='mult-section'>
+            <div>multiplication</div>
+
+        </div>)
+    }
+}
---
+++ src/components/Sum.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Sum extends Component{
+    render(){
+        return (<div className='sum-section'>
+             <div>summation</div>
+
+        </div>)
+    }
+}
