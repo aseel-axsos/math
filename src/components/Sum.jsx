92b6fa0ca6bc1966e80b7e6522b55b9140fcdc81
Rahaf Hosari <74103723+Rahafhosari@users.noreply.github.com>
2022-06-07 15:39:28 +0300

    aseel changes

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Sum extends Component{
     render(){
         return (<div className='sum-section'>
-             <div>summation</div>
+            <div>summation</div>
             <div>my changes on summ file</div>
 
             <div> Aseel - my changes on summ file</div>
